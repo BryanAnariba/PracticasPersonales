16719b20a453eba7f58940dfa7174edac14c862f
bryanariba <saariel115@gmail.com>
2022-11-09 05:57:17 -0600

    EMPEZAMOS CON MERN

--- MEAN/00-spotify/frontend/src/app/app.module.ts
+++ MEAN/00-spotify/frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+import { AlbumComponent } from './components/album/album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    PlaylistComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
