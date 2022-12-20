fdd6472e3b9430eef0c373bacfcc2ea3e093a2c3
BryanAnariba <43623601+BryanAnariba@users.noreply.github.com>
2022-12-19 18:44:38 -0600

    Api rest mysql node ts intro

---
+++ RestServers/01-api-rest-items-mysql/src/app.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import 'colors';
+import express, { Application } from 'express';
+import cors from 'cors';
+
+// Settings
+const PORT: number | string = process.env.PORT || 3501;
+const app: Application = express();
+app.set( 'PORT', process.env.PORT );
+
+// Middlewares
+app.use( cors() );
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+// Routes
+
+// Static Files
+
+// Start Server
+app.listen( app.get( 'PORT' ), () => {
+    console.clear();
+    console.log( `============================`.cyan );
+    console.log( `Server started on port: ${ app.get( 'PORT' ) }`.magenta );
+    console.log( `============================`.cyan );
+});
